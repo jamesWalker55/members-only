eb0c59ed34009e4ea69e995636a5f02edd06a978
James Walker <james.chunho+github@gmail.com>
2021-03-06 16:06:10 +0000

    Add name to Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,6 @@ class Member < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  validates :name, presence: true
 end
