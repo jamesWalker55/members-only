1e2b4c489ecd066d53d25cbb2e0e24d4e096c806
James Walker <james.chunho+github@gmail.com>
2021-03-06 15:26:31 +0000

    link member and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_150403) do
+ActiveRecord::Schema.define(version: 2021_03_06_152451) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_150403) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_id"
   end
 
 end
